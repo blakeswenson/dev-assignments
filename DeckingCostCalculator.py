f6e046adcc27f20ea58b338a9a0f6f47595aaba1
blakeswenson <blake.swenson@tcu.edu>
2025-02-11 10:30:41 -0600

    Update DeckingCostCalculator.py
    Made changes at request of professor on variable names, if statements, and title

--- DeckingCostCalculator.py
+++ DeckingCostCalculator.py
@@ -1,20 +1,27 @@
-## DECKING COST CALCULATOR 
+## Program: Decking Cost Calculator
+## Date: 2/11/2025
+## Author: Blake Swenson
+## Purpose: Allow a user to calculate the cost of a deck based on the square footage and the type of decking material they choose.
 
-pricePerSquareFoot = {
+price_per_square_foot = {
     'lumber': 2.35,
     'redwood': 7.75,
     'composite': 8.50,
 }
 
-print('DECKING COST CALCULATOR') ## Print the title of the application first
-squareFootage = int(input('Enter the square footage of the deck: ')) ## Ask the user to enter the square footage of the deck
+print('Welcome to Decking Cost Calculator!') ## Print the title of the application first
+square_footage = int(input('Enter the square footage of the deck: ')) ## Ask the user to enter the square footage of the deck
 
-print('Select the type of decking material:')  ## Tell the user the types of decking material
-materialType = input('Lumber, Redwood, or Composite: ').lower() ## Ask the user to enter the type of decking material
-if materialType not in pricePerSquareFoot: ## Check if the user entered a valid material type
+if square_footage > 0: ## Check if the user entered a valid square footage
+    print('Type the type of decking material:')  ## Tell the user the types of decking material
+    material_type = input('Lumber ($2.35 sq/ft), Redwood ($7.75 sq/ft), or Composite ($8.50 sq/ft): ').lower() ## Ask the user to enter the type of decking material
+    if material_type not in price_per_square_foot: ## Check if the user entered a valid material type
+        print('Invalid entry')
+        print('Did you enter lumber, redwood, or composite? Please, try again.') ## Print an error message if the user entered an invalid material type
+    else:
+        cost = price_per_square_foot[material_type] * square_footage ## Calculate the cost of the deck
+        print(f'{square_footage} square feet of {material_type} decking will cost ${cost:.2f}') ## Print the cost of the deck
+        print('Thank you!') ## Print a thank you message
+else:
     print('Invalid entry')
-    print('Did you enter lumber, redwood, or composite? Please, try again.')
-    exit() ## Handle invalid user selection
-
-cost = pricePerSquareFoot[materialType] * squareFootage ## Calculate the cost of the deck
-print(f'{squareFootage} square feet of {materialType} decking will cost ${cost:.2f}') ## Print the cost of the deck
+    print('Did you enter a whole number greater than zero? Please, try again.') ## Print an error message if the user entered an invalid square footage
